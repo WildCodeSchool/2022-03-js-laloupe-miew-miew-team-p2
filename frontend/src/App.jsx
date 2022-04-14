b79408dd834f7d2d51779d7386b1b4a2a94751e3
Thomas Lavo <99907399+Taiyo09k@users.noreply.github.com>
2022-04-14 15:17:03 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-// eslint-disable-next-line import/no-unresolved
-import "@pages/sass/home.scss";
 
 function App() {
   return (
