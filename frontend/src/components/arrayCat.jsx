32f292ffd985eca1548a210aa6340154f3d79266
anolibyssdev <loicag854@gmail.com>
2022-04-28 16:50:01 +0200

    style css de la barre de filtre, des détails des chats, modification du code filtre car problème avec eslint

---
+++ frontend/src/components/arrayCat.jsx
@@ -0,0 +1,43 @@
+// const arrayCat = [
+//   {
+//     image:
+//       "https://www.wanimo.com/veterinaire/wp-content/uploads/2018/11/chat-jaloux-e1574672723199.jpg",
+//     name: "Chat 1",
+//     taille: 30,
+//     poids: 11,
+//     poils: "long",
+//   },
+//   {
+//     image:
+//       "https://www.wanimo.com/veterinaire/wp-content/uploads/2018/11/chat-jaloux-e1574672723199.jpg",
+//     name: "Chat 2",
+//     taille: 32,
+//     poids: 12,
+//     poils: "cour",
+//   },
+//   {
+//     image:
+//       "https://www.wanimo.com/veterinaire/wp-content/uploads/2018/11/chat-jaloux-e1574672723199.jpg",
+//     name: "Chat 3",
+//     taille: 33,
+//     poids: 13,
+//     poils: "mi-long",
+//   },
+//   {
+//     image:
+//       "https://www.wanimo.com/veterinaire/wp-content/uploads/2018/11/chat-jaloux-e1574672723199.jpg",
+//     name: "Chat 4",
+//     taille: 34,
+//     poids: 14,
+//     poils: "mi-cour",
+//   },
+//   {
+//     image:
+//       "https://www.wanimo.com/veterinaire/wp-content/uploads/2018/11/chat-jaloux-e1574672723199.jpg",
+//     name: "Chat 5",
+//     taille: 35,
+//     poids: 15,
+//     poils: "chauve",
+//   },
+// ];
+// export default arrayCat;
---
+++ frontend/src/pages/Meow-who.jsx
@@ -0,0 +1,73 @@
+// eslint-disable-next-line import/no-unresolved
+import CatsCard from "@components/CatsCard";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./Meow-who.css";
+
+export default function MeowWho() {
+  const [arrayCat, setArrayCat] = useState([]);
+  function getArrayCat() {
+    const cats = [];
+    axios
+      .get("https://api.api-ninjas.com/v1/cats?children_friendly=5", {
+        headers: {
+          "X-Api-Key": "EeDQiPwn2Dx8mH16Xzs44Q==Qd2I6TdRSwScySwH",
+        },
+      })
+      .then((response) => response.data)
+      .then((data) => {
+        cats.push(...data);
+        axios
+          .get("https://api.api-ninjas.com/v1/cats?family_friendly=5", {
+            headers: {
+              "X-Api-Key": "EeDQiPwn2Dx8mH16Xzs44Q==Qd2I6TdRSwScySwH",
+            },
+          })
+          .then((response) => response.data)
+          .then((data1) => {
+            cats.push(...data1);
+
+            setArrayCat(cats);
+          });
+      });
+  }
+  useEffect(() => {
+    getArrayCat();
+  }, []);
+  const [filter, setFilter] = useState("");
+
+  return (
+    <div className="meowwho-page">
+      <h1 className="title-meow-page">Bienvenue sur Meow-page</h1>
+      <h2 className="sous-title-meow-page">
+        Découvrez nos chats selon leurs critères
+      </h2>
+      <div className="search">
+        <h3>Search by Name : </h3>
+        <input
+          type="text"
+          id="name-filter"
+          value={filter}
+          name="name"
+          placeholder="Write here the name of a cat"
+          onChange={(e) => setFilter(e.target.value)}
+        />
+      </div>
+
+      <section className="cards">
+        {filter === "" &&
+          arrayCat.map((cat) => {
+            return <CatsCard key={cat.name} cat={cat} />;
+          })}
+        {filter !== "" &&
+          arrayCat
+            .filter((cat) =>
+              cat.name.toLowerCase().includes(filter.toLowerCase())
+            )
+            .map((cat) => {
+              return <CatsCard key={cat.name} cat={cat} />;
+            })}
+      </section>
+    </div>
+  );
+}
