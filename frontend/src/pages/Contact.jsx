ab64b1c5dd5539ceb4adb00cce73ccd3b863d165
Erwan <e_fillaud@yahoo.fr>
2022-04-28 16:44:34 +0200

    adding contact page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,21 +1,31 @@
 import React, { useState } from "react";
 import "./contact.css";
 
+// const INITIAL_LIST = ["Learn React", "Learn Firebase", "Learn GraphQL"];*/
 export default function Contact() {
   const [email, setEmail] = useState("");
   const [fullName, setFullName] = useState("");
   const [message, setMessage] = useState("");
-
+  // const [value, setValue] = useState("");
+  // const [list, setList] = useState(INITIAL_LIST);
   const handleFullNameChange = (e) => setFullName(e.target.value);
   const handleEmailChange = (e) => setEmail(e.target.value);
   const handleMessageChange = (e) => setMessage(e.target.value);
-
+  // const handleSubmit = (event) => {
+  // if (value) {
+  // setList(list.concat(value));
+  // }
+  // setValue("");
+  // event.preventDefault();
+  // };
   return (
-    <form>
+    <form name="contact" method="post" data-netlify="true" onSubmit="submit">
+      <input type="hidden" name="form-name" value="contact" />
       <h2>Lets us improve the cat fight experience</h2>
       <h3>Name :</h3>
       <input
-        className="my_inpput"
+        name="name"
+        className="input-contact"
         id="fullName"
         type="text"
         value={fullName}
@@ -23,6 +33,8 @@ export default function Contact() {
       />
       <h4>Email :</h4>
       <input
+        name="email"
+        className="input-contact"
         id="email"
         type="email"
         value={email}
@@ -30,6 +42,8 @@ export default function Contact() {
       />
       <h5>Message :</h5>
       <input
+        name="message"
+        className="input-contact"
         id="message"
         type="message"
         value={message}
