2490bf62deabdc0042b6e9bfd2d50cbe11180e8d
anolibyssdev <loicag854@gmail.com>
2022-04-29 10:37:06 +0200

    style image card, fixer les class pour h2

--- src/components/CatsCard.jsx
+++ src/components/CatsCard.jsx
@@ -2,13 +2,15 @@ export default function CatsCard({ cat }) {
   return (
     <section className="card">
       <img src={cat.image_link} alt={cat.name} className="image-chat" />
-      <h2>{cat.name}</h2>
+      <h2 className="cat-name">{cat.name}</h2>
       <div className="details">
-        <h2>Origin : {cat.origin}</h2>
-        <h2>Family Friendly : {cat.family_friendly}</h2>
-        <h2>Length : {cat.length}</h2>
-        <h2>Min Weight: {cat.min_weight}</h2>
-        <h2>Children Friendly : {cat.children_friendly}</h2>
+        <h2 className="cat-details">Origin : {cat.origin}</h2>
+        <h2 className="cat-details">Family Friendly : {cat.family_friendly}</h2>
+        <h2 className="cat-details">Length : {cat.length}</h2>
+        <h2 className="cat-details">Min Weight: {cat.min_weight}</h2>
+        <h2 className="cat-details">
+          Children Friendly : {cat.children_friendly}
+        </h2>
       </div>
     </section>
   );
