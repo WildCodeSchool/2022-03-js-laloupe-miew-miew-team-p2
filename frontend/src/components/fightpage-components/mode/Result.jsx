15fba84ddc90964d4cc609abdd3b4f3dec921ebd
Kevlmt <97613786+Kevlmt@users.noreply.github.com>
2022-05-03 17:03:00 +0200

    css finished + new coldown on skills

--- frontend/src/components/fightpage-components/mode/Result.jsx
+++ frontend/src/components/fightpage-components/mode/Result.jsx
@@ -4,6 +4,7 @@ const Result = ({ winner, onSelectClick }) => {
   return (
     <div className="result">
       <h1 className="winner-name">{winner.name} has won !</h1>
+      <img className="winner-img" src={winner.image} alt="winner cat" />
       <button type="button" className="play-again-btn" onClick={onSelectClick}>
         Play Again.
       </button>
