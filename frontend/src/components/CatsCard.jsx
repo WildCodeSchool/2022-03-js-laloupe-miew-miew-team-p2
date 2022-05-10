8404649c65dd77c121a3118494e79ac02653273f
anolibyssdev <loicag854@gmail.com>
2022-05-10 14:23:16 +0200

    amélioré filtres, css sur card-cats, css sur portail, effect hover sur image-cat, ajout de button fermeture sur detail-card

--- frontend/src/components/CatsCard.jsx
+++ frontend/src/components/CatsCard.jsx
@@ -1,17 +1,21 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable import/no-unresolved */
+import Portail from "@components/Portail";
+import { useState } from "react";
+
 export default function CatsCard({ cat }) {
+  const [display, setDisplay] = useState(false);
   return (
     <section className="card">
-      <img src={cat.image_link} alt={cat.name} className="image-chat" />
+      <img
+        onClick={() => setDisplay(true)}
+        src={cat.image_link}
+        alt={cat.name}
+        className="image-chat"
+      />
       <h2 className="cat-name">{cat.name}</h2>
-      <div className="details">
-        <h2 className="cat-details">Origin : {cat.origin}</h2>
-        <h2 className="cat-details">Family Friendly : {cat.family_friendly}</h2>
-        <h2 className="cat-details">Length : {cat.length}</h2>
-        <h2 className="cat-details">Min Weight: {cat.min_weight}</h2>
-        <h2 className="cat-details">
-          Children Friendly : {cat.children_friendly}
-        </h2>
-      </div>
+      {display && <Portail cat={cat} setDisplay={setDisplay} />}
     </section>
   );
 }
--- frontend/src/pages/Meow-who.jsx
+++ frontend/src/pages/Meow-who.jsx
@@ -2,6 +2,7 @@
 // eslint-disable-next-line import/no-unresolved
 import CatsCard from "@components/CatsCard";
 import { useState, useEffect } from "react";
+
 import axios from "axios";
 import "./Meow-who.css";
 
@@ -10,7 +11,7 @@ export default function MeowWho() {
   function getArrayCat() {
     const cats = [];
     axios
-      .get("https://api.api-ninjas.com/v1/cats?children_friendly=5", {
+      .get("https://api.api-ninjas.com/v1/cats?family_friendly=5", {
         headers: {
           "X-Api-Key": "EeDQiPwn2Dx8mH16Xzs44Q==Qd2I6TdRSwScySwH",
         },
@@ -19,11 +20,14 @@ export default function MeowWho() {
       .then((data) => {
         cats.push(...data);
         axios
-          .get("https://api.api-ninjas.com/v1/cats?family_friendly=5", {
-            headers: {
-              "X-Api-Key": "EeDQiPwn2Dx8mH16Xzs44Q==Qd2I6TdRSwScySwH",
-            },
-          })
+          .get(
+            "https://api.api-ninjas.com/v1/cats?family_friendly=5&offset=20",
+            {
+              headers: {
+                "X-Api-Key": "EeDQiPwn2Dx8mH16Xzs44Q==Qd2I6TdRSwScySwH",
+              },
+            }
+          )
           .then((response) => response.data)
           .then((data1) => {
             cats.push(...data1);
@@ -36,15 +40,23 @@ export default function MeowWho() {
     getArrayCat();
   }, []);
   const [filter, setFilter] = useState("");
+  const [mode, setMode] = useState("");
+
+  useEffect(() => {
+    if (filter === "") {
+      setMode("");
+    } else {
+      setMode("name");
+    }
+  }, [filter]);
 
   return (
     <div className="meowwho-page">
-      <h1 className="title-meow-page">Bienvenue sur Meow-page</h1>
       <h2 className="sous-title-meow-page">
-        Découvrez nos chats selon leurs critères
+        Discover our cats according to their characteristics
       </h2>
       <div className="search">
-        <h3>Search by Name : </h3>
+        <h2 className="search-title">Search by Name : </h2>
         <input
           type="text"
           id="name-filter"
@@ -54,17 +66,75 @@ export default function MeowWho() {
           onChange={(e) => setFilter(e.target.value)}
         />
       </div>
+      <div className="filter-buttons-container">
+        <button
+          className={
+            mode === "max-weight" ? "button-filter-active3" : "button-filter"
+          }
+          type="button"
+          onClick={() => {
+            setMode(mode !== "max-weight" ? "max-weight" : "");
+          }}
+        >
+          FATTEST
+        </button>
+        <button
+          className={
+            mode === "playfulness" ? "button-filter-active2" : "button-filter"
+          }
+          type="button"
+          onClick={() => {
+            setMode(mode !== "playfulness" ? "playfulness" : "");
+          }}
+        >
+          PLAYFULLNESS
+        </button>
+        <button
+          className={
+            mode === "children-friendly"
+              ? "button-filter-active"
+              : "button-filter"
+          }
+          type="button"
+          onClick={() => {
+            setMode(mode !== "children-friendly" ? "children-friendly" : "");
+          }}
+        >
+          CHILDREN-FRIENDLY
+        </button>
+      </div>
 
       <section className="cards">
-        {filter === "" &&
+        {mode === "" &&
           arrayCat.map((cat) => {
             return <CatsCard key={cat.name} cat={cat} />;
           })}
-        {filter !== "" &&
+        {mode === "name" &&
           arrayCat
             .filter((cat) =>
               cat.name.toLowerCase().includes(filter.toLowerCase())
             )
+            .map((cat) => {
+              return <CatsCard key={cat.name} cat={cat} />;
+            })}
+
+        {mode === "max-weight" &&
+          arrayCat
+            .filter((cat) => cat.max_weight > 15)
+            .map((cat) => {
+              return <CatsCard key={cat.name} cat={cat} />;
+            })}
+
+        {mode === "playfulness" &&
+          arrayCat
+            .filter((cat) => cat.playfulness === 5)
+            .map((cat) => {
+              return <CatsCard key={cat.name} cat={cat} />;
+            })}
+
+        {mode === "children-friendly" &&
+          arrayCat
+            .filter((cat) => cat.children_friendly === 4)
             .map((cat) => {
               return <CatsCard key={cat.name} cat={cat} />;
             })}
